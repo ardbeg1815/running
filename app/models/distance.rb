f6e9d77945ab359d4bec5cfcdf2ed716649ea02e
ardbeg1815 <106896090+ardbeg1815@users.noreply.github.com>
2023-03-24 05:59:04 +0900

    Update distance.rb

--- app/models/distance.rb
+++ app/models/distance.rb
@@ -1,8 +1,8 @@
 class Distance < ApplicationRecord
 
-  validates :month,    presence: true
-  validates :date,     presence: true
-  validates :distance, presence: true
-  validates :weight,   presence: true
+  #validates :month,    presence: true
+  #validates :date,     presence: true
+  #validates :distance, presence: true
+  #validates :weight,   presence: true
 
 end
