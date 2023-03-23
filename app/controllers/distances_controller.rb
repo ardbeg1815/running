90f6c0148b267f4c2156694545dcd4a1dbd71c4b
ardbeg1815 <106896090+ardbeg1815@users.noreply.github.com>
2023-03-24 01:02:06 +0900

    Update distances_controller.rb

--- app/controllers/distances_controller.rb
+++ app/controllers/distances_controller.rb
@@ -1,5 +1,25 @@
 class DistancesController < ApplicationController
+
   def index
     @distances = Distance.all
   end
+
+  def new
+    @distance = Distance.new
+  end
+
+  def create
+    Distance.create(distance_params)
+  end
+
+  def destroy
+    distance = Distance.find(params[:id])
+    distance.destroy
+  end
+
+  private
+  def distance_params
+    params.require(:distance).permit(:month, :date, :distance, :weight)
+  end
+
 end
