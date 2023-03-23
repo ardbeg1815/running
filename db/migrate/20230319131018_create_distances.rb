1b585a6980bbf13770262445a4e15010ee6acd77
ardbeg1815 <106896090+ardbeg1815@users.noreply.github.com>
2023-03-24 01:01:53 +0900

    Update 20230319131018_create_distances.rb

--- db/migrate/20230319131018_create_distances.rb
+++ db/migrate/20230319131018_create_distances.rb
@@ -1,7 +1,10 @@
 class CreateDistances < ActiveRecord::Migration[6.0]
   def change
     create_table :distances do |t|
-      t.string :name
+      t.integer :month
+      t.integer :date
+      t.integer :distance
+      t.integer :weight
       t.timestamps
     end
   end
