d1b5c042ea94c767f5753ad9f9b2e67878fe78b5
ardbeg1815 <106896090+ardbeg1815@users.noreply.github.com>
2023-03-26 20:52:10 +0900

    Update distance.rb

--- app/models/distance.rb
+++ app/models/distance.rb
@@ -1,8 +1,8 @@
 class Distance < ApplicationRecord
 
-  validates :month,    numericality: {less_than_or_equal_to: 12}
-  validates :date,     numericality: {less_than_or_equal_to: 31}
-  validates :distance, numericality: {greater_than: 0}
-  validates :weight,   numericality: {greater_than: 0}
+  validates :month,    numericality: {less_than_or_equal_to: 12, message: ": 半角数字1〜12を入力してください"}
+  validates :date,     numericality: {less_than_or_equal_to: 31, message: ": 半角数字1〜31を入力してください"}
+  validates :distance, numericality: {greater_than: 0, message: ": 半角数字で入力してください"}
+  validates :weight,   numericality: {greater_than: 0, message: ": 半角数字で入力してください"}
 
 end
